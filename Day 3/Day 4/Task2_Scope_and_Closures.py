87ca186682bd78f8a2cfa3ac4686536dfe9ee4cf
shanavahora524-eng <shanavahora524@gmail.com>
2026-07-01 14:51:07 +0530

    Shana

---
+++ Day 3/Day 4/Task2_Scope_and_Closures.py
@@ -0,0 +1,17 @@
+def make_greeting(language):
+    
+    def greet(name):
+        if language=="Gujrati":
+            print("kem cho,",name)
+        elif language=="English":
+            print("Hello,",name)
+        else:
+            print("Hii,",name)
+        return
+    return greet
+
+Gujrati_greet=make_greeting("Gujrati")   
+English_greet=make_greeting("English")
+
+Gujrati_greet("Shana")
+English_greet("Shana")
