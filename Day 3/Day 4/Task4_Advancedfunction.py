d504d63f17d93132bb71c42f925bc947a4445738
shanavahora524-eng <shanavahora524@gmail.com>
2026-07-01 15:22:42 +0530

    shana

--- Day 3/Day 4/Task4_Advancedfunction.py
+++
@@ -1,5 +0,0 @@
-username=["Shana22","Aksha21","Aafiya29","Mahin16","ilma","Zeinab"]
-valid_username=list(filter(lambda name:len(name)>=6,username))
-
-print("username with at least 6 character:")
-print(valid_username)
